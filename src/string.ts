4f8862dc5d0d94c8235104fab205bb46a38651f6
Shigma <1700011071@pku.edu.cn>
2019-11-30 18:23:39 +0800

    remove edit distance

--- src/string.ts
+++ src/string.ts
@@ -1,23 +1,3 @@
-export function editDistance (str1: string, str2: string) {
-  let lastRow = new Array(str2.length + 1).fill(0).map((_, index) => index)
-  for (let i = 1; i <= str1.length; ++i) {
-    const thisRow = [i]
-    for (let j = 1; j <= str2.length; ++j) {
-      thisRow.push(Math.min(
-        thisRow[j - 1] + 1,
-        lastRow[j] + 1,
-        lastRow[j - 1] + Number(str1[i - 1] !== str2[j - 1]),
-      ))
-    }
-    lastRow = thisRow
-  }
-  return lastRow[str2.length]
-}
-
-export function findSimilar (target: string) {
-  return (name: string) => name.length > 2 && editDistance(name, target) <= name.length * 0.4
-}
-
 function deepen (modifyString: (source: string) => string) {
   function modifyObject <T> (source: T): T {
     if (typeof source !== 'object' || !source) return source
