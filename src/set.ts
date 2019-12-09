347b4eefd6875e9bc4c4ce26088fc4b277453599
Shigma <1700011071@pku.edu.cn>
2019-12-09 17:23:34 +0800

    use readonly array

--- src/set.ts
+++ src/set.ts
@@ -1,15 +1,15 @@
-export function isSubset (array1: any[], array2: any[]) {
+export function isSubset (array1: readonly any[], array2: readonly any[]) {
   return array1.every(item => array2.includes(item))
 }
 
-export function intersection <T> (array1: T[], array2: T[]) {
+export function intersection <T> (array1: readonly T[], array2: readonly T[]) {
   return array1.filter(item => array2.includes(item))
 }
 
-export function complement <S> (array1: S[], array2: any[]) {
-  return array1.filter(item => !array2.includes(item as any))
+export function difference <S> (array1: readonly S[], array2: readonly any[]) {
+  return array1.filter(item => !array2.includes(item))
 }
 
-export function union <T> (array1: T[], array2: T[]) {
+export function union <T> (array1: readonly T[], array2: readonly T[]) {
   return Array.from(new Set([...array1, ...array2]))
 }
